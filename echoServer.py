21c0c0fbb48efd270e842829d844770d23695bf1
Christina Nguyen <phuonghien716@gmail.com>
2015-10-02 20:35:46 -0400

    Added exceptions to server code

--- echoServer.py
+++ echoServer.py
@@ -2,18 +2,31 @@
 # -*- coding: utf-8 -*- 
 
 """ 
-A simple echo server 
+Nathaniel Hughes
+Christina Nguyen
+
+ECE 4564, Fall 2015
+Assignment 1
 """
 
 import socket
 
 host = ''
 port = 50000
-backlog = 5
+backlog = 1 # One client will be conencting to the server
 size = 1024
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((host,port))
-s.listen(backlog)
+
+s = None
+try:
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Uses IPv4 and TCP
+	s.bind((host,port))
+	s.listen(backlog) 
+except socet.error, (value, message):
+	if s:
+		s.close()
+	print "Could not open socket: " + message
+	sys.exit(1)
+
 while 1:
      conn, addr = s.accept() 
      data = conn.recv(1024)
